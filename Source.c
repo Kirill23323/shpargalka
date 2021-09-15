8ef1f225f14b4f70c0fbee8bea3cd77e506c5534
Kirill23323 <90023656+Kirill23323@users.noreply.github.com>
2021-09-15 10:35:24 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+	char b = 'a';
+	unsigned char c = 'c';
+	short d = 3232;
+	unsigned short f = 60000;
+	int i = 123424;
+	unsigned int n = 32453;
+	long y = 24266;
+	unsigned long m = 352523;
+	float v = 3.56;
+	double g = 324.563;
+	long double U = 3245.43;
+
+
+
+
+	a = 0;
+
+	printf("%d %d", a, b);
+}
