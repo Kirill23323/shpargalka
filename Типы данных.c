a98d8bd05bf0cc3c3403cb6ef48290f0beeebe27
Kirill23323 <90023656+Kirill23323@users.noreply.github.com>
2021-09-18 17:18:53 +0300

    Add files via upload

---
+++ Типы данных.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int a = 1234;
+	long b  = 124444;
+	char c = 'C';
+	unsigned char k = 5;
+	short d = 12425;
+	unsigned short e = 60000;
+	unsigned int f = 2145235;
+	unsigned long g = 245235;
+	float h = 12.425f;
+	double i = 34.53634636;
+	long double j = 1234.4687364879628793646367;
+	printf("Peremennie: %d %ld %c %uc %hd %hu %u %lu %f %f %lf\n",a, b, c, k, d, e, f, g, h, i, j);
+
+	return 0;
+
+
+}
